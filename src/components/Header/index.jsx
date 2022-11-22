4de9ed95deaf117a73f81f8625ed244409012edb
elmadiF <84844658+madielSan@users.noreply.github.com>
2022-11-22 15:15:02 +0100

    Conversion du logo Kasa en lien pour retourner dans la page d'accueil + suppression des fichiers inutiles

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,8 @@ import '../../_scss/layouts/header.scss'
 function Header() {
     return(
         <div className='navbar'>
-            <Logo className='navbar__logo' />
+            <NavLink to="/"><Logo className='navbar__logo' /></NavLink>
+            
             <nav className='navbar__nav'>
                 <NavLink className={({ isActive }) => isActive ? "navbar__nav__link active" : "navbar__nav__link"} to="/" end>Accueil</NavLink>
                 <NavLink className={({ isActive }) => isActive ? "navbar__nav__link active" : "navbar__nav__link"}  to="/about">A propos</NavLink>
